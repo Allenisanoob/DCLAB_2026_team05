4a37a528ace78470dec40c0bb42532f04dbd3b68
elitian01180118-cmyk <elitian01180118@gmail.com>
2026-05-09 17:12:18 +0800

    Update Player.sv

--- Final/src/Player.sv
+++ Final/src/Player.sv
@@ -11,6 +11,7 @@ module Player (
     localparam SEND = 1'd1;
 
     logic        state_r, state_w;
+    logic        is_init_r, is_init_w;
     logic [3:0]  counter_r, counter_w;
     logic [15:0] o_DAC_DAT_r, o_DAC_DAT_w;
     logic        daclrck_prev_r;
@@ -23,11 +24,16 @@ module Player (
         state_w         = state_r;
         counter_w       = counter_r;
         o_DAC_DAT_w     = o_DAC_DAT_r;
+        is_init_w       = is_init_r;
         o_B2P_request_l = 1'b0; 
         o_B2P_request_r = 1'b0; 
 
         case(state_r)
             IDLE: begin
+                if (is_init_r && i_DAC_LRCK) o_B2P_request_l = 1'b1;
+                else if(is_init_r && !i_DAC_LRCK) o_B2P_request_r = 1'b1;
+                is_init_w       = 1'b0;
+            end
                 if (lrc_edge) begin
                     state_w = SEND;
                     o_DAC_DAT_w = i_out_data;
@@ -49,11 +55,13 @@ module Player (
     always_ff @(negedge i_BCLK or negedge i_rst) begin
         if (!i_rst) begin
             state_r        <= IDLE;
+            is_init_r      <= 1'b1;
             counter_r      <= 4'd0;
             o_DAC_DAT_r    <= 16'd0;
             daclrck_prev_r <= i_DAC_LRCK;
         end else begin
             state_r        <= state_w;
+            is_init_r      <= is_init_w;
             counter_r      <= counter_w;
             o_DAC_DAT_r    <= o_DAC_DAT_w;
             daclrck_prev_r <= i_DAC_LRCK;
