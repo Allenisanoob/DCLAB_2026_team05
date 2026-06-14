73d31704c67dbc63e7e24c45dc0c240214d398b1
lsf0613 <shufanlee99@gmail.com>
2026-06-14 16:31:33 +0800

    fix overflow

--- Final/src/blocks/Schroeder_Allpass.sv
+++ Final/src/blocks/Schroeder_Allpass.sv
@@ -1,41 +1,42 @@
 module Schroeder_Allpass #(
-    parameter integer delay_sample = 2304,
-    parameter signed [7:0] gain = 8'sd64 // Q1.7 (-1 ~ 1)
+    parameter integer delay_sample = 2304
 )(
     input clk,
     input rst,
     input in_valid,
     input signed [15:0] in,
-    // input signed [7:0] gain, // Q1.7
-    output out_valid,
-    output signed [15:0] out
+    input signed [7:0] gain, // Q1.7
+    output reg out_valid,
+    output reg signed [15:0] out
 );
 
 logic [15:0] v_buffer [0:delay_sample - 1];
 logic [$clog2(delay_sample) - 1:0] ptr;
 logic is_loop;
-logic signed [15:0] temp_0; // temp_0 = v_buffer[ptr] // Q15.0
-logic signed [15:0] v_delay;
+logic signed [15:0] temp_0; // temp_0 = v_buffer[ptr] // Q16.0
+logic signed [15:0] v_delay; // Q16.0
 logic signed [23:0] temp_1; // temp_1 = gain * v_delay // Q17.7
-logic signed [16:0] temp_2; // temp_2 = (temp_1 + 24'sd64) >>> 7 // Q16.0
-logic signed [15:0] v_curr; // v_curr = in - temp_2 // Q15.0
+logic signed [16:0] temp_2; // temp_2 = (temp_1 + 24'sd64) >>> 7 // Q17.0
+logic signed [17:0] v_curr_temp; // v_curr_temp = in - temp_2 // Q18.0
+logic signed [15:0] v_curr; // v_curr = saturate(v_curr_temp) // Q16.0
 logic signed [23:0] temp_3; // temp_3 = gain * v_curr // Q17.7
-logic signed [15:0] temp_4; // temp_4 = (temp_3 + 24'sd64) >>> 7 // Q16.0
-logic signed [15:0] out_curr; // out_curr = temp_4 + v_delay // Q15.0
-
-logic out_valid_;
-logic signed [15:0] out_;
-
-assign out_valid = out_valid_;
-assign out = out_;
+logic signed [16:0] temp_4; // temp_4 = (temp_3 + 24'sd64) >>> 7 // Q17.0
+logic signed [17:0] out_curr_temp; // out_curr_temp = temp_4 + v_delay // Q18.0
+logic signed [15:0] out_curr; // out_curr = saturate(out_curr_temp) // Q16.0
 
 assign v_delay = is_loop ? temp_0 : 16'sd0;
 assign temp_1 = gain * v_delay;
 assign temp_2 = (temp_1 + 24'sd64) >>> 7;
-assign v_curr = in - temp_2;
+assign v_curr_temp = in - temp_2;
+assign v_curr = (v_curr_temp > 18'sd32767) ? 18'sd32767 :
+                (v_curr_temp < -18'sd32768) ? -18'sd32768 :
+                v_curr_temp[15:0];
 assign temp_3 = gain * v_curr;
 assign temp_4 = (temp_3 + 24'sd64) >>> 7;
-assign out_curr = temp_4 + v_delay;
+assign out_curr_temp = temp_4 + v_delay;
+assign out_curr = (out_curr_temp > 18'sd32767) ? 18'sd32767 :
+                  (out_curr_temp < -18'sd32768) ? -18'sd32768 :
+                  out_curr_temp[15:0];
 
 always_ff @(posedge clk or negedge rst) begin
     if (!rst) begin
@@ -49,10 +50,10 @@ always_ff @(posedge clk or negedge rst) begin
             ptr <= ptr + 1;
         end
         v_buffer[ptr] <= v_curr;
-        out_ <= out_curr;
-        out_valid_ <= 1'b1;
+        out <= out_curr;
+        out_valid <= 1'b1;
     end else if (out_valid) begin
-        out_valid_ <= 1'b0;
+        out_valid <= 1'b0;
         temp_0 <= v_buffer[ptr];
     end
 end
