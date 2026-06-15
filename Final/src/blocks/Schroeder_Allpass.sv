271b1b022a4126a8994056f47c5c69fdfb0bc904
lsf0613 <shufanlee99@gmail.com>
2026-06-15 21:05:44 +0800

    Add files via upload

--- Final/src/blocks/Schroeder_Allpass.sv
+++ Final/src/blocks/Schroeder_Allpass.sv
@@ -11,6 +11,7 @@ module Schroeder_Allpass #(
 );
 
 logic signed [7:0] gain_r;
+logic out_valid_pipe;
 
 logic [15:0] v_buffer [0:delay_sample - 1];
 logic [$clog2(delay_sample) - 1:0] ptr;
@@ -21,6 +22,7 @@ logic signed [23:0] temp_1; // temp_1 = gain * v_delay // Q17.7
 logic signed [16:0] temp_2; // temp_2 = (temp_1 + 24'sd64) >>> 7 // Q17.0
 logic signed [17:0] v_curr_temp; // v_curr_temp = in - temp_2 // Q18.0
 logic signed [15:0] v_curr; // v_curr = saturate(v_curr_temp) // Q16.0
+logic signed [15:0] v_curr_pipe;
 logic signed [23:0] temp_3; // temp_3 = gain * v_curr // Q17.7
 logic signed [16:0] temp_4; // temp_4 = (temp_3 + 24'sd64) >>> 7 // Q17.0
 logic signed [17:0] out_curr_temp; // out_curr_temp = temp_4 + v_delay // Q18.0
@@ -33,7 +35,7 @@ assign v_curr_temp = in - temp_2;
 assign v_curr = (v_curr_temp > 18'sd32767) ? 18'sd32767 :
                 (v_curr_temp < -18'sd32768) ? -18'sd32768 :
                 v_curr_temp[15:0];
-assign temp_3 = gain_r * v_curr;
+assign temp_3 = gain_r * v_curr_pipe;
 assign temp_4 = (temp_3 + 24'sd64) >>> 7;
 assign out_curr_temp = temp_4 + v_delay;
 assign out_curr = (out_curr_temp > 18'sd32767) ? 18'sd32767 :
@@ -45,6 +47,8 @@ always_ff @(posedge clk or negedge rst) begin
         ptr <= 0;
         is_loop <= 0;
         gain_r <= 0;
+        out_valid_pipe <= 1'b0;
+        v_curr_pipe <= 16'sd0;
     end else if (in_valid) begin
         gain_r <= gain;
         if (ptr == delay_sample - 1) begin
@@ -54,8 +58,12 @@ always_ff @(posedge clk or negedge rst) begin
             ptr <= ptr + 1;
         end
         v_buffer[ptr] <= v_curr;
+        v_curr_pipe <= v_curr;
+        out_valid_pipe <= 1'b1;
+    end else if (out_valid_pipe) begin
         out <= out_curr;
         out_valid <= 1'b1;
+        out_valid_pipe <= 1'b0;
     end else if (out_valid) begin
         gain_r <= gain;
         out_valid <= 1'b0;
