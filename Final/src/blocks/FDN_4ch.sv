efdb322921410f25849b0a6df0a4223f097648e2
lsf0613 <shufanlee99@gmail.com>
2026-06-15 20:12:52 +0800

    Add files via upload

--- Final/src/blocks/FDN_4ch.sv
+++ Final/src/blocks/FDN_4ch.sv
@@ -8,12 +8,9 @@ module FDN_4ch #(
     input clk,
     input rst,
     input in_valid,
-    input signed [15:0] in_0,
-    input signed [15:0] in_1,
-    input signed [15:0] in_2,
-    input signed [15:0] in_3,
+    input signed [15:0] in,
     output reg out_valid,
-    output signed reg [15:0] out
+    output reg signed [15:0] out
 );
 
 logic signed [15:0] ch0_buffer [0:delay_sample_0 - 1];
@@ -36,10 +33,10 @@ logic signed [15:0] ch0_delay; // ch0_delay = is_loop_0 ? ch0_read : 16'sd0
 logic signed [15:0] ch1_delay; // ch1_delay = is_loop_1 ? ch1_read : 16'sd0
 logic signed [15:0] ch2_delay; // ch2_delay = is_loop_2 ? ch2_read : 16'sd0
 logic signed [15:0] ch3_delay; // ch3_delay = is_loop_3 ? ch3_read : 16'sd0
-logic signed [23:0] ch0_temp_0; // ch0_temp_0 = gain * ch0_delay // Q16.8
-logic signed [23:0] ch1_temp_0; // ch1_temp_0 = gain * ch1_delay // Q16.8
-logic signed [23:0] ch2_temp_0; // ch2_temp_0 = gain * ch2_delay // Q16.8
-logic signed [23:0] ch3_temp_0; // ch3_temp_0 = gain * ch3_delay // Q16.8
+logic signed [17:0] ch0_temp_0; // ch0_temp_0 = gain * ch0_delay // Q16.8
+logic signed [17:0] ch1_temp_0; // ch1_temp_0 = gain * ch1_delay // Q16.8
+logic signed [17:0] ch2_temp_0; // ch2_temp_0 = gain * ch2_delay // Q16.8
+logic signed [17:0] ch3_temp_0; // ch3_temp_0 = gain * ch3_delay // Q16.8
 logic signed [25:0] ch0_temp_1; // ch0_temp_1 = ch0_temp_0 + ch1_temp_0 + ch2_temp_0 + ch3_temp_0 // Q17.9
 logic signed [25:0] ch1_temp_1; // ch1_temp_1 = ch0_temp_0 - ch1_temp_0 + ch2_temp_0 - ch3_temp_0 // Q17.9
 logic signed [25:0] ch2_temp_1; // ch2_temp_1 = ch0_temp_0 + ch1_temp_0 - ch2_temp_0 - ch3_temp_0 // Q17.9
@@ -56,29 +53,28 @@ logic signed [15:0] ch0_curr; // ch0_curr = saturate(ch0_curr_temp) // Q16.0
 logic signed [15:0] ch1_curr; // ch1_curr = saturate(ch1_curr_temp) // Q16.0
 logic signed [15:0] ch2_curr; // ch2_curr = saturate(ch2_curr_temp) // Q16.0
 logic signed [15:0] ch3_curr; // ch3_curr = saturate(ch3_curr_temp) // Q16.0
-logic signed [17:0] out_temp_0; // out_temp_0 = ch0_delay + ch1_delay + ch2_delay + ch3_delay // Q18.0
-logic signed [15:0] out_temp_1; // out_temp_1 = out_temp_0 >>> 2 // Q16.0
+logic signed [17:0] out_temp; // out_temp = ch0_temp_0 >>> 2 // Q16.0
 
 assign ch0_delay = is_loop_0 ? ch0_read : 16'sd0;
 assign ch1_delay = is_loop_1 ? ch1_read : 16'sd0;
 assign ch2_delay = is_loop_2 ? ch2_read : 16'sd0;
 assign ch3_delay = is_loop_3 ? ch3_read : 16'sd0;
-assign ch0_temp_0 = gain * ch0_delay;
-assign ch1_temp_0 = gain * ch1_delay;
-assign ch2_temp_0 = gain * ch2_delay;
-assign ch3_temp_0 = gain * ch3_delay;
-assign ch0_temp_1 = ch0_temp_0 + ch1_temp_0 + ch2_temp_0 + ch3_temp_0;
-assign ch1_temp_1 = ch0_temp_0 - ch1_temp_0 + ch2_temp_0 - ch3_temp_0;
-assign ch2_temp_1 = ch0_temp_0 + ch1_temp_0 - ch2_temp_0 - ch3_temp_0;
-assign ch3_temp_1 = ch0_temp_0 - ch1_temp_0 - ch2_temp_0 + ch3_temp_0;
+assign ch0_temp_0 = ch0_delay + ch1_delay + ch2_delay + ch3_delay;
+assign ch1_temp_0 = ch0_delay - ch1_delay + ch2_delay - ch3_delay;
+assign ch2_temp_0 = ch0_delay + ch1_delay - ch2_delay - ch3_delay;
+assign ch3_temp_0 = ch0_delay - ch1_delay - ch2_delay + ch3_delay;
+assign ch0_temp_1 = gain * ch0_temp_0;
+assign ch1_temp_1 = gain * ch1_temp_0;
+assign ch2_temp_1 = gain * ch2_temp_0;
+assign ch3_temp_1 = gain * ch3_temp_0;
 assign ch0_temp_2 = (ch0_temp_1 + 26'sd256) >>> 9;
 assign ch1_temp_2 = (ch1_temp_1 + 26'sd256) >>> 9;
 assign ch2_temp_2 = (ch2_temp_1 + 26'sd256) >>> 9;
 assign ch3_temp_2 = (ch3_temp_1 + 26'sd256) >>> 9;
-assign ch0_curr_temp = in_0 + ch0_temp_2;
-assign ch1_curr_temp = in_1 + ch1_temp_2;
-assign ch2_curr_temp = in_2 + ch2_temp_2;
-assign ch3_curr_temp = in_3 + ch3_temp_2;
+assign ch0_curr_temp = in + ch0_temp_2;
+assign ch1_curr_temp = in + ch1_temp_2;
+assign ch2_curr_temp = in + ch2_temp_2;
+assign ch3_curr_temp = in + ch3_temp_2;
 assign ch0_curr = (ch0_curr_temp > 18'sd32767) ? 18'sd32767 :
                   (ch0_curr_temp < -18'sd32768) ? -18'sd32768 :
                   ch0_curr_temp[15:0];
@@ -91,9 +87,7 @@ assign ch2_curr = (ch2_curr_temp > 18'sd32767) ? 18'sd32767 :
 assign ch3_curr = (ch3_curr_temp > 18'sd32767) ? 18'sd32767 :
                   (ch3_curr_temp < -18'sd32768) ? -18'sd32768 :
                   ch3_curr_temp[15:0];                  
-assign out_temp_0 = ch0_delay + ch1_delay + ch2_delay + ch3_delay;
-assign out_temp_1 = out_temp_0 >>> 2;
-
+assign out_temp = ch0_temp_0 >>> 2;
 always_ff @(posedge clk or negedge rst) begin
     if (!rst) begin
         ptr_0 <= 0;
@@ -134,7 +128,7 @@ always_ff @(posedge clk or negedge rst) begin
         ch2_buffer[ptr_2] <= ch2_curr;
         ch3_buffer[ptr_3] <= ch3_curr;
         out_valid <= 1'b1;
-        out <= out_temp_1;
+        out <= out_temp;
     end else if (out_valid) begin
         out_valid <= 0;
         ch0_read <= ch0_buffer[ptr_0];
